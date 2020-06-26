902478af44a81a092787decc74398e041d80e453
Josiah <reikiryo@yahoo.com>
2020-06-26 16:21:41 -0400

    MVP

--- src/App.js
+++ src/App.js
@@ -1,20 +1,66 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Grid from './components/Grid'
 import GridForm from './components/GridForm'
 
+const starter = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+const starter2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 function App() {
-    const [gameArr, setArr] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
-    const point = 10
-    const toggleGame = () => {
+    const [gameArr, setArr] = useState([...starter2])
+    const [len, setLen] = useState(625)
+    const [point, setPoint] = useState(25)
 
+    // useEffect(() => {
+    //     const arr = []
+    //     console.log('on it')
+    //     for (let i = 0; i < point + 2; i++) {
+    //         arr.push(2)
+    //     }
+    //     for (let i = 0; i < len / point; i++) {
+    //         arr.push(2)
+    //         for (let i = 0; i < point; i++) {
+    //             arr.push(0)
+    //         }
+    //         arr.push(2)
+    //     }
+    //     for (let i = 0; i < point + 2; i++) {
+    //         arr.push(2)
+    //     }
+    //     console.log('done')
+    //     setArr(arr)
+    // }, [len])
+
+    const handleSubmit = (e) => {
+        // e.preventDefault()
+        const arr = []
+        console.log('on it')
+        for (let i = 0; i < point + 2; i++) {
+            arr.push(2)
+        }
+        for (let i = 0; i < len / point; i++) {
+            arr.push(2)
+            for (let i = 0; i < point; i++) {
+                arr.push(0)
+            }
+            arr.push(2)
+        }
+        for (let i = 0; i < point + 2; i++) {
+            arr.push(2)
+        }
+        console.log('done')
+        setArr(arr)
     }
-    // const toggleGridItem = (item, index) => {
-    //     let list = [...gameArr]
-    //     list[index] = item
-    //     setArr(list)
-    // }
+
+    // useEffect(() => {
+    //     handleSubmit()
+    // }, [])
+
     return (
-        <div className="App">
+        <div className="container">
+            <form onSubmit={e => e.preventDefault()}>
+                <input onChange={e => setLen(e.target.value)} name='len' type='number' value={len} />
+                <input onChange={e => setPoint(e.target.value)} name='point' type='number' value={point} />
+                <button onClick={handleSubmit}>Submit</button>
+            </form>
             <Grid arr={gameArr} point={point} />
         </div>
     );
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import GridItem from './GridItem'
-// array of user input length, array starts in state user sets
-// break point which specifies how to access the array
-// 
+import useInterval from '../hooks/useInterval'
 
 const Grid = ({ arr, point }) => {
     const [gridArr, setGrid] = useState(arr)
     const [canToggle, setToggle] = useState(true)
     const [auto, setAuto] = useState(false)
+    const [delay, setDelay] = useState(1000)
+    const [gen, setGen] = useState(0)
+    const [skip, setSkip] = useState(0)
+    const i = useInterval((e) => { if (auto === true) { resolveFrame() } }, delay)
 
     const checkItem = (list, index, point) => {
         let arr = [...list]
@@ -34,11 +36,12 @@ const Grid = ({ arr, point }) => {
         }
     }
 
-    const resolveFrame = () => {
+    const resolveFrame = (e) => {
         let arr = [...gridArr]
         setGrid(arr.map((item, idx) => {
             return checkItem(arr, idx, point)
         }))
+        setGen(gen + 1)
         setToggle(false)
     }
 
@@ -53,34 +56,55 @@ const Grid = ({ arr, point }) => {
     }
 
     const resetGrid = (e) => {
+        setAuto(false)
         setGrid(arr)
+        setToggle(true)
+        setGen(0)
     }
 
     const toggleGame = (e) => {
         setAuto(!auto)
     }
 
-    useEffect(() => {
-        while (auto === true) {
-            setTimeout(() => {
-                resolveFrame()
-            }, 100)
+    const skipGen = async (skips) => {
+        console.log(skips)
+        let arr = [...gridArr]
+        for (let i = 0; i < skips; i++) {
+            console.log('skipping')
+            arr = arr.map((item, idx) => {
+                return checkItem(arr, idx, point)
+            })
         }
-    }, [auto])
-
+        setGrid(arr)
+        setGen(gen + skips)
+        setToggle(false)
+    }
 
     return (
-        <div className='grid'>
-            {gridArr.map((item, idx) => {
-                if (idx !== 0 && (idx + 1) % point === 0) {
-                    return <><GridItem toggle={toggleGridItem} canToggle={canToggle} status={item} index={idx} /><br /></>
-                } else {
-                    return <GridItem toggle={toggleGridItem} canToggle={canToggle} status={item} index={idx} />
-                }
-            })}
-            <button onClick={toggleGame}>{auto ? 'Stop' : 'Start'}</button>
-            <button onClick={e => resolveFrame()}>Step</button>
-            <button onClick={resetGrid}>Reset</button>
+        <div className='container'>
+            <h2>{`Generation ${gen}`}</h2>
+            <div className='grid'>
+                {gridArr.map((item, idx) => {
+                    if (idx !== 0 && (idx + 1) % (point) === 0) {
+                        return <><GridItem key={idx} toggle={toggleGridItem} canToggle={canToggle} status={item} index={idx} /><br /></>
+                    } else {
+                        return <GridItem key={idx} toggle={toggleGridItem} canToggle={canToggle} status={item} index={idx} />
+                    }
+                })}
+                {/*gridArr.map((item, idx) => {
+
+                }) */}
+            </div>
+            <div className='inputs'>
+                <button onClick={toggleGame}>{auto ? 'Stop' : 'Start'}</button>
+                <button onClick={resolveFrame}>Step</button>
+                <button onClick={resetGrid}>Reset</button>
+                <input onChange={e => setDelay(e.target.value)} name='delay' type='number' value={delay} />
+            </div>
+            <div>
+                <input onChange={e => setSkip(e.target.value)} type='number' name='skip' value={skip} />
+                <button onClick={e => skipGen(parseInt(skip))}>Skip</button>
+            </div>
         </div>
     )
 }
